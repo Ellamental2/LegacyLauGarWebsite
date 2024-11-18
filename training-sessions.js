a1ecb991795e66401bfd81c6128ea4f7c66f28e5
ellam fox <ellammalle_1994@hotmail.com>
2024-11-18 18:15:11 +0000

    Update Christmas Break dates format in training-sessions.js to DD/MM/YYYY

--- training-sessions.js
+++ training-sessions.js
@@ -16,9 +16,9 @@ const trainingPlan = {
 const specialWeeks = {
     //"05/12/2023": "Open Session - Swords and Sticks",
     // Add more special dates if needed.
-    "12/16/2024": "Christmas Break (No Training)",
-    "12/23/2024": "Christmas Break (No Training)",
-    "12/30/2024": "Christmas Break (No Training)",
+    "16/12/2024": "Christmas Break (No Training)",
+    "23/12/2024": "Christmas Break (No Training)",
+    "30/12/2024": "Christmas Break (No Training)",
 };
 
 const otherEvents = [ //american date format mm/dd/yyyy
